d2de7791bd2f5ad4f259b710c5dc0e076c6117ac
IlliaLinetskyi <illia.linetskyi@stripo.email>
2025-06-19 12:57:37 +0300

    process_sales dag creation

---
+++ airflow/process_sales.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='sales_etl_pipeline',
+    description='ETL pipeline for sales: raw → bronze → silver',
+    default_args=default_args,
+    start_date=datetime(2025, 6, 18),
+    schedule_interval=None,  
+    catchup=False,
+) as dag:
+
+    raw_to_bronze = AwsGlueJobOperator(
+        task_id='sales_raw_to_bronze',
+        job_name='sales raw-bronze',      
+        aws_conn_id='aws_default',        
+        region_name='eu-north-1',       
+        wait_for_completion=True,
+        verbose=True,
+    )
+
+    bronze_to_silver = AwsGlueJobOperator(
+        task_id='sales_bronze_to_silver',
+        job_name='sales bronze-silver',   
+        aws_conn_id='aws_default',
+        region_name='eu-central-1',
+        wait_for_completion=True,
+        verbose=True,
+    )
+
+    raw_to_bronze >> bronze_to_silver
